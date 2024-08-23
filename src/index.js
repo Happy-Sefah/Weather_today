84ea13aa5ad64454c970384ef45da3716a64de15
Happy-Sefah <happisos@gmail.com>
2024-08-22 23:47:39 -0400

    Added real-time temp change

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
-function searchSubmit (event) {
+function refreshWeatherData(response) {
+    let tempElement = document.querySelector("#city-temp-value");
+    tempElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+    let apiKey = "4078o345b3e8ef8e68dbfe19dta5019e";
+    let apiURL = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiURL).then(refreshWeatherData);
+}
+
+function searchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
