9468ac621abdca4fa4823577b8626555bc74d6b1
Happy-Sefah <happisos@gmail.com>
2024-08-30 15:45:57 -0400

    Injected forecast HTML from js

--- src/index.js
+++ src/index.js
@@ -56,7 +56,35 @@ function searchSubmit(event) {
     searchCity(searchInput.value);
 }
 
-let searchFormElement = document.querySelector("#search-form");
+function displayForecast() {
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat",];
+  let forecastHtml = "";
+
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌥️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>18º</strong>
+          </div>
+          <div class="weather-forecast-temperature">11º</div>
+        </div>
+      </div>
+    `;
+  });
+
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = forecastHtml;
+}
+
+  let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", searchSubmit);
 
 searchCity("Paris");
+displayForecast();
+
+
